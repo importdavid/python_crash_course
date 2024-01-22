8f793d39102d08c8365ae1ff74a4d579d8c4d955
David Wilkins <wilkidav@gmail.com>
2024-01-21 20:36:01 -0500

    forgot to add 1 function

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -24,19 +24,18 @@ class AlienInvasion:
             self._update_screen()
             self.clock.tick(60)
 
-            # Redraw the screen during each pass through the loop.
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-
-            # Make the most recently drawn screen visible.
-            pygame.display.flip()
-            self.clock.tick(60)
-
     def _check_events(self):
         """Respond to keypresses and mouse events."""
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+    
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        pygame.display.flip()
 
 
 if __name__ == '__main__':
