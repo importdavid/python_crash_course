bec8e2471998a100e29450ea4ffa086c37cb8db6
David Wilkins <wilkidav@gmail.com>
2024-01-29 19:28:49 -0500

    using powershell for chap15 work

--- data_visualization/mpl_squares.py
+++ data_visualization/mpl_squares.py
@@ -1,9 +1,18 @@
 import matplotlib.pyplot as plt
 
-squares = [x**2 for x in range(1,6)]
-# print(squares)
+input_values = list(range(1,6))
+squares = [x**2 for x in input_values]
 
+plt.style.use('seaborn-v0_8-darkgrid')
 fig, ax = plt.subplots()
-ax.plot(squares)
+ax.plot(input_values, squares, linewidth=3)
+
+# Set chart title and lavel axes.
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
+
+# Set size of tick labels.
+ax.tick_params(labelsize=14)
 
 plt.show()
