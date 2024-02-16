7835c6afad1c0bb3917ba5a2c0f47e69285ff589
David Wilkins <wilkidav@gmail.com>
2024-02-16 14:29:52 -0500

    added edit_entry functionality

--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -59,5 +59,18 @@ def new_entry(request, topic_id):
 
 def edit_entry(request, entry_id):
     """Edit an existing entry."""
-    entry = Entry.object
-    # Continue here! pg 412
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != 'POST':
+        # Initial request; pre-fill form with the current entry.
+        form = EntryForm(instance=entry)
+    else:
+        # POST data submitted; process data.
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topic', topic_id=topic.id)
+        
+    context = {'entry': entry, 'topic':topic, 'form': form}
+    return render(request, 'learning_logs/edit_entry.html', context)
